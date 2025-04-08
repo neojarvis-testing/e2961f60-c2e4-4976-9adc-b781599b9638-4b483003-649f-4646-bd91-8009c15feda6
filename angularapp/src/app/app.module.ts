eae2dea22faa6b53fe884ea685e332e49d4327cd
Srushtibankar01 <srushtib01@gmail.com>
2025-04-08 09:47:22 +0000

    added app.module

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,9 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
+import { UsermakeorderComponent } from './components/usermakeorder/usermakeorder.component';
+import { UsernavComponent } from './components/usernav/usernav.component';
+import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
+import { UserviewfoodComponent } from './components/userviewfood/userviewfood.component';
+import { UserviewordersComponent } from './components/uservieworders/uservieworders.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    RegistrationComponent,
+    UseraddfeedbackComponent,
+    UsermakeorderComponent,
+    UsernavComponent,
+    UserviewfeedbackComponent,
+    UserviewfoodComponent,
+    UserviewordersComponent
   ],
   imports: [
     BrowserModule,
