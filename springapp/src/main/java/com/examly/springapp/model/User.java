29322efd55f50639b232b1a51d177242711fb09e
ThilakManiChand <reachthilak08@gmail.com>
2025-04-08 13:09:48 +0000

    Commit 4 - UserModel - UserRepo - 6.40 - 8.04.25

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -1,5 +1,27 @@
 package com.examly.springapp.model;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    private String email;
+    private String password;
+    private String mobileNumber;
+    private String userRole;
 }
