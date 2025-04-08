023635aac09b0b7412eecacc25dbbf35eb4ad8d5
Dev-D02 <devang.doke@gmail.com>
2025-04-08 13:01:46 +0000

    Added Food And Feedback Model

--- springapp/src/main/java/com/examly/springapp/model/Feedback.java
+++ springapp/src/main/java/com/examly/springapp/model/Feedback.java
@@ -1,5 +1,40 @@
 package com.examly.springapp.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int feedbackId;
+    private String feedbackText;
+    private LocalDate date;
+    private int rating;
+
+    @ManyToOne
+    @JoinColumn(name="userId",nullable=false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="foodId",nullable=false)
+    private Food food;
 
 }
--- springapp/src/main/java/com/examly/springapp/model/Food.java
+++ springapp/src/main/java/com/examly/springapp/model/Food.java
@@ -1,5 +1,35 @@
 package com.examly.springapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int foodId;
+    private String foodName;
+    private Double price;
+    private int stockQuantity;
+    private byte[] photo;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable=false)
+    private User user;
 
 }
