19239bb49482c49d8115e4017e342580b7775a16
ThilakManiChand <reachthilak08@gmail.com>
2025-04-08 09:53:51 +0000

    Commit1-8.4.25-3.15-components

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminaddfoodComponent } from './components/adminaddfood/adminaddfood.component';
+import { AdminnavComponent } from './components/adminnav/adminnav.component';
+import { AdminorderschartComponent } from './components/adminorderschart/adminorderschart.component';
+import { AdminviewfeedbackComponent } from './components/adminviewfeedback/adminviewfeedback.component';
+import { AdminviewordersComponent } from './components/adminvieworders/adminvieworders.component';
+import { AuthguardComponent } from './components/authguard/authguard.component';
+import { ErrorComponent } from './components/error/error.component';
+import { AdminviewfoodComponent } from './components/adminviewfood/adminviewfood.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminaddfoodComponent,
+    AdminnavComponent,
+    AdminorderschartComponent,
+    AdminviewfeedbackComponent,
+    AdminviewordersComponent,
+    AuthguardComponent,
+    ErrorComponent,
+    AdminviewfoodComponent,
+  
   ],
   imports: [
     BrowserModule,
---
+++ angularapp/src/app/components/adminvieworders/adminvieworders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminvieworders',
+  templateUrl: './adminvieworders.component.html',
+  styleUrls: ['./adminvieworders.component.css']
+})
+export class AdminviewordersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
