3e4d9bd64bd5bd3415e0a2ff0a27fae3fdfdf0c4
dubile414 <dubilekrishna132@gmail.com>
2025-04-08 13:00:05 +0000

    08-04-25 ---- Orders model updated

--- springapp/src/main/java/com/examly/springapp/model/Orders.java
+++ springapp/src/main/java/com/examly/springapp/model/Orders.java
@@ -1,5 +1,41 @@
 package com.examly.springapp.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Orders {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int orderId;
+    private String orderStatus;
+    private double totalAmount;
+    private int quantity;
+    private LocalDate orderDate;
+
+    @ManyToOne
+    @JoinColumn(name = "userId" , nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "foodId", nullable = false)
+    private Food food;
+    
 }
