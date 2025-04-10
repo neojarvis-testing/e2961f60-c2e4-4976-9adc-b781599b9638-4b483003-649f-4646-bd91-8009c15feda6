97d02e7c824d6e0b100f28f49ca297de28468788
Srushtibankar01 <srushtib01@gmail.com>
2025-04-10 13:30:00 +0000

    Implement food components for Angular App

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -9,16 +9,19 @@ import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserviewfoodComponent } from './components/userviewfood/userviewfood.component';
+import { AdmineditfoodComponent } from './components/admineditfood/admineditfood.component';
+import { AdminaddfoodComponent } from './components/adminaddfood/adminaddfood.component';
 
 const routes: Routes = [
+  {path:'adminaddfood', component:AdminaddfoodComponent},
   {path:'adminvieworders',component:AdminviewordersComponent},
-  
-  { path: 'adminviewfood', component: AdminviewfoodComponent },
+  {path:'adminviewfood', component: AdminviewfoodComponent },
   {path:'login',component:LoginComponent},
   {path:'register',component:RegistrationComponent},
   {path:'home',component:HomeComponent},
   {path:'admin/view/foods',component:AdminviewfoodComponent},
-  {path:'user/view/foods',component:UserviewfoodComponent}
+  {path:'user/view/foods',component:UserviewfoodComponent},
+  {path:'admineditfood',component:AdmineditfoodComponent}
 
 ];
 
--- angularapp/src/app/components/adminviewfood/adminviewfood.component.ts
+++ angularapp/src/app/components/adminviewfood/adminviewfood.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Food } from 'src/app/models/food.model';
+import { FoodService } from 'src/app/services/food.service';
 
 @Component({
   selector: 'app-adminviewfood',
@@ -6,10 +9,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adminviewfood.component.css']
 })
 export class AdminviewfoodComponent implements OnInit {
+  foods: any[] = [];
+  filteredFoods: any[] = [];
+  searchName: string = '';
+  minPrice: number | null = null;
+  maxPrice: number | null = null;
+  foodId : number = 1;
 
-  constructor() { }
+  constructor(private foodService: FoodService, private router : Router) { }
 
   ngOnInit(): void {
+    this.foodService.getAllFoods().subscribe((data) => {
+        this.foods = data;
+        this.filteredFoods = data;
+      },
+      (error) => {
+        console.error('Error fetching food details:', error);
+      }
+    );
   }
 
-}
+  public filterFoods(): void {
+    this.filteredFoods = this.foods.filter((food) => {
+      const matchesName = food.name.toLowerCase().includes(this.searchName.toLowerCase());
+      const matchesPrice =
+        (this.minPrice === null || food.price >= this.minPrice) &&
+        (this.maxPrice === null || food.price <= this.maxPrice);
+      return matchesName && matchesPrice;
+    });
+  }
+
+  public resetFilters(): void {
+    this.searchName = '';
+    this.minPrice = null;
+    this.maxPrice = null;
+    this.filteredFoods = [this.foods]; 
+  }
+
+  confirmDelete(foodId: number): void {
+    const confirmation = window.confirm('Are you sure you want to delete this food item?');
+  
+    if (confirmation) {
+      this.foodService.deleteFood(foodId).subscribe(response => {
+          console.log('Food item deleted successfully');
+          this.ngOnInit(); 
+        },
+        error => {
+          console.error('Error occurred while deleting food:', error);
+        }
+      );
+    } else {
+      console.log('Deletion cancelled');
+    }
+  }
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
