6975d1471c1e6ddccd371560725dbdb973dd2540
Srushtibankar01 <srushtib01@gmail.com>
2025-04-10 04:14:17 +0000

    Added food controller(updated)

--- springapp/src/main/java/com/examly/springapp/controller/FoodController.java
+++ springapp/src/main/java/com/examly/springapp/controller/FoodController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examly.springapp.exceptions.FoodNotFoundException;
+import com.examly.springapp.exceptions.UserNotFoundException;
 import com.examly.springapp.model.Food;
 import com.examly.springapp.service.FoodService;
 
@@ -80,6 +82,40 @@ public class FoodController {
             return ResponseEntity.status(403).body(i.getMessage());
         }
     
+    }
+
+    @DeleteMapping("/api/food/{foodId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteFood (@PathVariable int id){
+        try{
+            boolean isDeleted=foodService.deleteFood(id);
+            if(isDeleted) {
+                return ResponseEntity.status(200).body("Food with ID " + id + " deleted successfully.");
+            }else {
+                return ResponseEntity.status(500).body("Failed to delete food with ID " + id);
+            }
+        }
+        catch(FoodNotFoundException e){
+            return ResponseEntity.status(403).body(e.getMessage());
+        }
+        catch(IllegalArgumentException i){
+            return ResponseEntity.status(403).body(i.getMessage());
+        }  
+    }
+
+    @GetMapping("/api/food/{userId}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> getFoodsByUserId (@PathVariable int userId){
+        try{
+            List<Food> foods = foodService.getFoodsByUserId(userId);
+            return ResponseEntity.status(200).body(foods);
+        }
+        catch(IllegalArgumentException i){
+            return ResponseEntity.status(403).body(i.getMessage());
+        } 
+        catch(UserNotFoundException e){
+            return ResponseEntity.status(403).body(e.getMessage());
+        }
     }
 }
 
