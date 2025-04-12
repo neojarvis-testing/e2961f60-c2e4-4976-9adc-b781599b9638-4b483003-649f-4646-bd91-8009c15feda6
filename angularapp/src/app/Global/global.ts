59ba8beaadb253e3101dc4900dddc976857f7980
dubile414 <dubilekrishna132@gmail.com>
2025-04-12 13:03:02 +0000

    All changes told by sir Done in Orders

--- angularapp/src/app/Global/global.ts
+++ angularapp/src/app/Global/global.ts
@@ -1,3 +1,3 @@
 export class url{
-    public static apiUrl:string = "https://ide-affbecdbcccfcdfbdefbbddcfebfcdbbfbdcfeda.premiumproject.examly.io/proxy/8080"
+    public static apiUrl:string = "https://ide-fbcaaeabbdebdefbbddcfebfcdbbfbdcfeda.premiumproject.examly.io/proxy/8080"
 }
